e9491650f78886bd7bdebc9ec6e9c26d4685301e
prosper collins <prospercollins19@gmail.com>
2021-09-23 00:15:25 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,76 @@
+<template>
+<div>
+<Header :totalIncome="state.totalIncome"/>
+<Form  @add-income="AddIncome"/>
+<IncomeList :state="state"/>
+</div>
+</template>
+
+<script>
+import { reactive, computed } from 'vue';
+import Header from './components/Header';
+import Form from './components/Form';
+import IncomeList from './components/IncomeList';
+ 
+  export default {
+     name: 'App',
+     components:{
+       Header,
+       Form,
+       IncomeList
+     },
+     setup() {
+      const state = reactive({
+        income:[],
+        totalIncome: computed(()=>{
+           let temp =0;
+           if(state.income.length >0 ){
+             for(let i=0; i<state.income.length; i++) {
+                  temp += state.income[i].value;
+
+             }
+           }
+           return temp;
+        }),
+        sortedIncome: computed(()=>{
+            let temp = [];
+            // eslint-disable-next-line vue/no-side-effects-in-computed-properties
+            temp = state.income.sort(function(a, b){
+                  return b.date - a.date; 
+            })
+            return temp;
+        })
+        
+      });
+      function AddIncome(data){
+        let d =data.date.split("-");
+        let newD = new Date(d[0], d[1], d[2]);
+        state.income = [...state.income,{
+          id: Date.now(),
+          desc: data.desc,
+          value: parseInt(data.value),
+          date: newD.getTime()
+        }];
+         
+      }
+       return {
+        state,
+        AddIncome 
+       }
+     }
+
+  }
+</script>
+
+<style scoped>
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: 'Fira Sans', sans-serif;
+}
+
+body{
+  background: red;
+}
+</style>
---
+++ src/components/IncomeList.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="income-list">
+    <IncomeItem  v-for="data in state.sortedIncome" :key="data.id" :income="data" />
+    </div>
+</template>
+
+<script>
+import IncomeItem from './IncomeItem'
+
+    export default {
+        name: 'Income',
+        components:{
+            IncomeItem
+        },
+        props:{
+            state: Object
+        }
+    }
+</script>
+
+<style lang="css" scoped>
+.income-list{
+margin-top:30px;
+padding: 15px;
+}
+
+</style>
